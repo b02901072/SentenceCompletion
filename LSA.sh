80c1bf0aba56de4251b2b9d9690c52d6cebfd333
joean8415 <b02901072@ntu.edu.tw>
2016-12-26 02:54:57 +0800

    new

--- LSA.sh
+++ LSA.sh
@@ -1,20 +1,20 @@
-MSR_TEST=data/MSR_Sentence_Completion_Challenge_V1/
-QUESTION=$MSR_TEST/Holmes.machine_format.questions.txt
+MSR_TEST=data/MSR_Sentence_Completion_Challenge_V1
+QUESTION=$MSR_TEST/Data/Holmes.machine_format.questions.txt
+ANSWER=$MSR_TEST/Data/Holmes.machine_format.answers.txt
 
-MODEL=model/
-WORD2VEC_MODEL=$model/key.125.model
+MODEL=model
+WORD2VEC_MODEL=$MODEL/key.125.model
 
 STOP_WORDS=data/stop_words.txt
 
 RESULT=$1
 
-FEATURE_1=data/neighbor.bi.1.csv
-FEATURE_2=data/neighbor.bi.2.csv
-
-ANSWER=data/testing/Data/Holmes.machine_format.answers.txt
+NEIGHBOR=data/neighbor
+FEATURE_1=$NEIGHBOR/neighbor.bi.1.csv
+FEATURE_2=$NEIGHBOR/neighbor.bi.2.csv
 
 if [ -f $RESULT ]; then
   rm $RESULT
 fi
 python src/LSA.py $WORD2VEC_MODEL $QUESTION $RESULT $FEATURE_1 $FEATURE_2 $STOP_WORDS
-./data/testing/score.pl $RESULT $ANSWER #&> /dev/null
+$MSR_TEST/score.pl $RESULT $ANSWER #&> /dev/null
